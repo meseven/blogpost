163a30990d6957138612e2b14f54c9525619ddcf
Mehmet Seven <mehmetseven0@gmail.com>
2017-10-02 23:01:51 +0300

    fix lint problem

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express')
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.json({ name: 'Mehmet', surname: 'Seven', age: 24, location: 'Istanbul' });
+	res.json({ name: 'Mehmet', surname: 'Seven', age: 24, location: 'Istanbul', job: 'jobless' });
 });
 
 module.exports = router;
