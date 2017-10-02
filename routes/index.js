c0a92d92ec16529bdef16bb34f64ffb656bd478f
Mehmet Seven <mehmetseven0@gmail.com>
2017-10-02 20:52:58 +0300

    eslint integration

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ name: 'Mehmet', surname: "Seven", age: 24 });
+router.get('/', function(req, res) {
+	res.json({ name: 'Mehmet', surname: 'Seven', age: 24 });
 });
 
 module.exports = router;
--- test/index.js
+++ test/index.js
@@ -1,21 +1,23 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+/*eslint-disable */
 let should = chai.should();
+/*eslint-enable */
 let server = require('../app');
 
 chai.use(chaiHttp);
 
 describe('Node Server', () => {
-    it('it should GET all the data', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('name').to.be.an('string');
-                res.body.should.have.property('surname').to.be.an('string');
-                res.body.should.have.property('age').to.be.an('number');
-                done();
-            });
-    });
+	it('it should GET all the data', (done) => {
+		chai.request(server)
+			.get('/')
+			.end((err, res) => {
+				res.should.have.status(200);
+				res.body.should.be.a('object');
+				res.body.should.have.property('name').to.be.an('string');
+				res.body.should.have.property('surname').to.be.an('string');
+				res.body.should.have.property('age').to.be.an('number');
+				done();
+			});
+	});
 });
