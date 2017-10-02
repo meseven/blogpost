8f2daf80d505b4dc4fdcc0ff4d09f1d6f1711679
Mehmet Seven <mehmetseven0@gmail.com>
2017-10-02 22:14:40 +0300

    new property

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.json({ name: 'Mehmet', surname: 'Seven', age: 24 });
+	res.json({ name: 'Mehmet', surname: 'Seven', age: 24, location: 'Istanbul' });
 });
 
 module.exports = router;
